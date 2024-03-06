3a6fb489e3c8ae93d6f80441328cbd49832e6a69
junior-zip <junior26bmx@gmail.com>
2024-03-06 16:02:22 -0300

    test: ensure signUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -73,6 +73,23 @@ describe('Signup Controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
     })
 
+    test('Should return 400 if password confirmation fails', () =>{
+
+        const {sut} = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com,',
+                password: 'any_password',
+                passwordConfirmation: 'invalid_password',
+                
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+    })
+
     test('Should return 400 if no confirmation is provided', () =>{
 
         const {sut} = makeSut()
